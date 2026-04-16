21db7950329ef7c3548b4bbd9ab73a8e14a7180f
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2026-04-16 17:50:26 +0200

    Add JS to hide filters by default and show/hide with buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+/**
+ * Searching for anything on the searchbar results in
+ * showing 3 fixed dummy tags.
+ * 
+ * Closing one tag closes all three.
+ */
 const input = document.getElementById("searchbar");
 const tags = document.getElementById("selected-tags");
 const closeTag = document.querySelectorAll(".close-btn");
@@ -22,3 +29,43 @@ closeTag.forEach(btn => {
     tags.classList.add("hidden");
   });
 });
+
+
+const filterMenu = document.getElementById("filter-menu-background");
+const closeFiltersButton = document.getElementById("close-filter-btn");
+const filterButton = document.getElementById("filter-btn");
+const applyFiltersButton = document.getElementById("apply-filters-btn");
+
+const showFilters = () => {
+  filterMenu.classList.remove("hidden");
+};
+
+filterButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  showFilters();
+});
+
+
+const closeFilters = () => {
+  filterMenu.classList.add("hidden");
+};
+
+closeFiltersButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeFilters();
+});
+
+
+/*
+
+const applyFilters = () => {
+  filterMenu.classList.add("hidden");
+};
+
+applyFiltersButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  //Gå till sökresultatet
+});
+
+*/
+
