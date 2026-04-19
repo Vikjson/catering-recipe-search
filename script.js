f8864208ba400682e1faa3911dc840005c300ada
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2026-04-19 23:19:53 +0200

    Add search result container, clear button and js to show/hide search results

--- script.js
+++ script.js
@@ -16,39 +16,28 @@ const showTags = () => {
 };
 
 
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    e.preventDefault();
-    showTags();
-  }
-});
-
-searchButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  showTags();
-});
-
+const closeAllTags = () => {
+  tags.classList.add("hidden");
+};
 
 closeTag.forEach(btn => {
-  btn.addEventListener("click", () => {
-    tags.classList.add("hidden");
-  });
+  btn.addEventListener("click", closeAllTags);
 });
 
 
-/**
- * Show filter menu with filter button
- * 
- * and close filters with X-button on filter menu.
- */
+
+
 const filterMenu = document.getElementById("filter-menu-background");
 const closeFiltersButton = document.getElementById("close-filter-btn");
 const filterButton = document.getElementById("filter-btn");
 const applyFiltersButton = document.getElementById("apply-filters-btn");
+const clearSearchButton = document.getElementById("clear-search-btn");
 
 const showFilters = () => {
   filterMenu.classList.remove("hidden");
 };
+
+
 
 filterButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -66,16 +55,57 @@ closeFiltersButton.addEventListener("click", (e) => {
 });
 
 
-/*
 
-const applyFilters = () => {
-  filterMenu.classList.add("hidden");
-};
+
+
 
 applyFiltersButton.addEventListener("click", (e) => {
   e.preventDefault();
-  //Gå till sökresultatet
+  closeFilters();
+  showSearchResults();
 });
 
-*/
 
+
+
+
+const searchResults = document.getElementById("search-results-recipes");
+const startPageRecipes = document.getElementById("recipes");
+
+
+const showSearchResults = () => {
+  startPageRecipes.classList.add("hidden");
+  searchResults.classList.remove("hidden");
+  tags.classList.remove("hidden");
+};
+
+const hideSearchResults = () => {
+  startPageRecipes.classList.remove("hidden");
+  searchResults.classList.add("hidden");
+  tags.classList.add("hidden");
+};
+
+
+closeTag.forEach(btn => {
+  btn.addEventListener("click", hideSearchResults);
+});
+
+
+clearSearchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  hideSearchResults();
+});
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  showTags();
+  showSearchResults();
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    showTags();
+    showSearchResults();
+  }
+});
